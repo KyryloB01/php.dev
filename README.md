c1febb86683a523c4304d44f2f5b7fe4b87f3d65
KyryloB01 <hillaztek@gmail.com>
2025-09-15 22:12:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# php.dev
+
+Local PHP project for lecture 21 (XAMPP, virtual host).
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<!doctype html><html><head><meta charset='utf-8'><title>php.dev</title></head><body>";
+echo "<h1>Hello from php.dev (local)</h1>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+echo "</body></html>";
